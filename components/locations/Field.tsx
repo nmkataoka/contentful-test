d67b0c88e16e0db1818338040a1380f41c6cd818
Nolan Kataoka <nolan.kataoka@grammarly.com>
2023-07-24 15:33:54 -0400

    feat: demonstrate build issue

--- components/locations/Field.tsx
+++ components/locations/Field.tsx
@@ -1,10 +1,12 @@
-import React from 'react';
-import { Paragraph } from '@contentful/f36-components';
-import { FieldAppSDK } from '@contentful/app-sdk';
-import { /* useCMA, */ useSDK } from '@contentful/react-apps-toolkit';
+import React, { useState } from "react";
+import { Paragraph } from "@contentful/f36-components";
+import { FieldAppSDK } from "@contentful/app-sdk";
+import { MultipleEntryReferenceEditor } from "@contentful/field-editor-reference";
+import { /* useCMA, */ useSDK } from "@contentful/react-apps-toolkit";
 
 const Field = () => {
   const sdk = useSDK<FieldAppSDK>();
+  const [state] = useState(false)
   /*
      To use the cma, inject it as follows.
      If it is not needed, you can remove the next line.
@@ -13,7 +15,25 @@ const Field = () => {
   // If you only want to extend Contentful's default editing experience
   // reuse Contentful's editor components
   // -> https://www.contentful.com/developers/docs/extensibility/field-editors/
-  return <Paragraph>Hello Entry Field Component (AppId: {sdk.ids.app})</Paragraph>;
+  if (state) {
+    return (
+      <MultipleEntryReferenceEditor
+        viewType="link"
+        sdk={sdk}
+        isInitiallyDisabled
+        hasCardEditActions
+        parameters={{
+          instance: {
+            showCreateEntityAction: true,
+            showLinkEntityAction: true,
+          },
+        }}
+      />
+    );
+  }
+  return (
+    <Paragraph>Hello Entry Field Component (AppId: {sdk.ids.app})</Paragraph>
+  );
 };
 
 export default Field;
